bc154b63790e4419b249650f9a9abd864144a9ca
Cesar <cesarbatistasantos08@gmail.com>
2026-05-18 08:49:48 -0300

    Mostra popup de ser possível interagir

--- scripts/ui/message_ui.gd
+++ scripts/ui/message_ui.gd
@@ -1,6 +1,14 @@
 extends Area2D
 
+@onready var icon = $InteractionIcon
+
 var player_near = false
+
+@export var message = "A porta está trancada."
+
+func _ready():
+
+	icon.visible = false
 
 func _process(delta):
 
@@ -8,14 +16,18 @@ func _process(delta):
 
 		var ui = get_tree().get_first_node_in_group("message_ui")
 
-		ui.show_message("Fiquei acordada até tarde ontem... Pensar tira o meu sono")
+		ui.show_message(message)
 
 func _on_body_entered(body):
 
 	if body.name == "Player":
+
 		player_near = true
+		icon.visible = true
 
 func _on_body_exited(body):
 
 	if body.name == "Player":
+
 		player_near = false
+		icon.visible = false
