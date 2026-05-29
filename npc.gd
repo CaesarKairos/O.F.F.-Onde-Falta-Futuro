8c35503d05517f7e6573bbf9b8c9a70494a51679
C3ss2k <cesarbatistasantos08@gmail.com>
2026-05-28 23:39:32 -0300

    Implementa base inicial do sistema de diálogos

---
+++ npc.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+@onready var icon = $InteractionIcon
+
+var player_near = false
+
+func _ready():
+
+	icon.visible = false
+
+func _process(delta):
+
+	if player_near and Input.is_action_just_pressed("interact"):
+
+		var ui = get_tree().get_first_node_in_group("message_ui")
+
+		ui.show_dialog(
+			"Olá viajante.",
+			[
+				"Quem é você?",
+				"Tchau"
+			]
+		)
+
+func _on_body_entered(body):
+
+	if body.name == "Player":
+
+		player_near = true
+		icon.visible = true
+
+func _on_body_exited(body):
+
+	if body.name == "Player":
+
+		player_near = false
+		icon.visible = false
--- scenes/ui/message_ui.gd
+++ scenes/ui/message_ui.gd
@@ -1,12 +1,43 @@
 extends CanvasLayer
 
-@onready var label = $Label
+@onready var panel = $Panel
+@onready var label = $Panel/Label
+@onready var choices = $Panel/ChoicesLabel
 
+var current_options = []
+
+func _ready():
+
+	panel.visible = false
+
+# TEXTO SIMPLES
 func show_message(texto):
 
+	panel.visible = true
+
 	label.text = texto
-	label.visible = true
+
+	choices.text = ""
 
 	await get_tree().create_timer(2).timeout
 
-	label.visible = false
+	panel.visible = false
+
+# DIÁLOGO COM OPÇÕES
+func show_dialog(texto, options):
+
+	panel.visible = true
+
+	label.text = texto
+
+	current_options = options
+
+	choices.text = ""
+
+	for i in range(options.size()):
+
+		choices.text += str(i + 1) + " - " + options[i] + "\n"
+
+func hide_dialog():
+
+	panel.visible = false
