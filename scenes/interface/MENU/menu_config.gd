1d086325e718b5668b86e649e2f7b75bbc72b8be
Nicolly França <nicollyalvesfporto@gmail.com>
2026-08-19 11:13:21 -0300

    feat: implementa navegação do menu de configuração e inicia menu de pausa

--- scenes/interface/MENU/menu_config.gd
+++ scenes/interface/MENU/menu_config.gd
@@ -1,10 +1,52 @@
 extends Node2D
 
+var pagina_atual := 1
+var camera_atual := 1
+
+
 func _ready():
-	print("MENU CONFIG INICIADO")
-	mostrar_pagina(1)
+	mostrar_pagina(pagina_atual)
+
 
 func mostrar_pagina(pagina: int):
-	$SETUP/pag1.visible = pagina == 1
-	$SETUP/pag2.visible = pagina == 2
-	$SETUP/pag3.visible = pagina == 3
+	pagina_atual = pagina
+	
+	$SETUP/pag1.hide()
+	$SETUP/pag2.hide()
+	$SETUP/pag3.hide()
+	
+	if pagina == 1:
+		$SETUP/pag1.show()
+	elif pagina == 2:
+		$SETUP/pag2.show()
+	elif pagina == 3:
+		$SETUP/pag3.show()
+
+
+func _on_setinha_direita_pressed() -> void:
+	if pagina_atual == 3:
+		mostrar_pagina(1)
+	else:
+		mostrar_pagina(pagina_atual + 1)
+
+
+func _on_setinha_esquerda_pressed() -> void:
+	if pagina_atual == 1:
+		mostrar_pagina(3)
+	else:
+		mostrar_pagina(pagina_atual - 1)
+
+
+func _on_cam_1_pressed() -> void:
+	mostrar_pagina(1)
+	print("CAM 1")
+
+
+func _on_cam_2_pressed() -> void:
+	mostrar_pagina(2)
+	print("CAM 2")
+
+
+func _on_cam_3_pressed() -> void:
+	mostrar_pagina(3)
+	print("CAM 3")
